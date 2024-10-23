e2115fd00a9e3e96aedaa47f4d6a5142ed1e713a
mdimamhosen <imamhosen9426224@gmail.com>
2024-10-23 15:00:32 +0600

    99% complete  commit

--- src/components/TagInput.jsx
+++ src/components/TagInput.jsx
@@ -50,5 +50,16 @@ const TagInput = ({ name, setTask, task }) => {
     </div>
   );
 };
+// Changes i made to the code are:
+// 1. I imported the useState and useEffect hooks from react.
+// 2. I created a TagInput component that takes three props: name, setTask, and task.
+// 3. I created a state variable selectedTag to store the selected tag.
+// 4. I created a list of available tags.
+// 5. I used the useEffect hook to set the selected tag when the task tags change.
+// 6. I created a handleAddTag function to add a tag to the selected tag state.
+// 7. I used the useEffect hook to update the task tags when the selected tag changes.
+// 8. I returned a div with a select element to add tags to the task.
+// 9. I mapped over the available tags to create options in the select element.
+// 10. I returned the TagInput component in the TaskForm component.
 
 export default TagInput;
